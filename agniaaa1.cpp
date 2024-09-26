53bacc6b95d1e664198b2bab685f7773a5c48628
abdiagnia <abdiagnia9@gmail.com>
2024-09-26 20:16:50 +0700

    Add files via upload

---
+++ agniaaa1.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+	char* nama = "siti nur agnia";
+	char* nim = "2441044";
+	char* jurusan = "teknik informatika";
+	
+	printf("nama: %s\n", nama);
+	printf("nim: %s\n", nim);
+	printf("jurusan: %s\n", jurusan);
+	return 0;
+}
---
+++ agniaaa2.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+	int panjang = 10;
+	int lebar = 7;
+	int luas = panjang * lebar;
+	
+	printf("panjang 10 cm \n", panjang);
+	printf("lebar 7 cm \n", lebar);
+	printf("luas persegi  panjang adalah: %d cm^2\n", luas);
+	return 0;	
+	
+}
+
